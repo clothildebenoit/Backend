28e7dfece9dba667144372ff361fe81959a957e4
27010-34-07 <27010-34-07@exchange.ad.afpanet>
2022-06-14 16:39:55 +0200

    14/06

---
+++ Menuiz PHP/Header_Footer/header.php
@@ -0,0 +1,23 @@
+<?php session_start();?>
+<header>
+        <nav>
+            <h3><?php 
+            if ((isset( $_SESSION["login"])) && (!empty( $_SESSION["login"]))){
+
+                 echo "Utilisateur" . $_SESSION["login"] ." "; 
+            } else {
+                 echo "Utilisateur non reconnu.";
+            }
+            
+           
+            
+            
+            
+            ?> </h3>
+            <a href="index.php">Accueil</a>
+           
+           
+           
+            <img class="logo headLogo" src="img\MenuizMan_logo.png" alt="logo">
+        </nav>
+</header
