f3829ec4b60763dfa09b0a448969bb03b0e8c4c2
clothilde <coty@gmx.fr>
2022-08-22 16:54:28 +0200

    22/08

---
+++ Le chantier 2.0/Le chantier 2.0/inscription.php
@@ -0,0 +1,47 @@
+<link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+<link rel="stylesheet" href="css/style.css">
+<!-- inscription -->
+
+<div class="main">
+    <section class="signup">
+        <div class="container">
+            <div class="signup-content">
+
+                <div class="signup-image">
+                <figure><img src="images/chantier.jpg" alt="chantier"></figure>
+                </div>
+
+                <div class="signup-form">
+                    <h2 class="form-title">Créer un compte</h2>
+                    <form method="POST" class="register-form" id="register-form">
+                    <div class="form-group">
+                        <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                        <input type="text" name="name" id="name" placeholder="Prénom"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="email"><i class="zmdi zmdi-email"></i></label>
+                        <input type="email" name="email" id="email" placeholder="Email"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                        <input type="password" name="pass" id="pass" placeholder="Mot de passe"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                        <input type="password" name="re_pass" id="re_pass" placeholder="répétez le mot de passe"/>
+                    </div>
+                    <!-- <div class="form-group">
+                        <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                        <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                    </div>
+                    <div class="form-group form-button">
+                        <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+                    </div> -->
+                    </form>
+                </div>
+            
+            </div>
+        </div>
+
+    </section>
+</div>
---
+++ exo dialogue/dialogue/dialogue.php
@@ -0,0 +1,20 @@
+<?php
+$mysqli = new Mysqli('localhost', 'root', '', 'dialogue');
+
+if($_POST)
+{
+    echo "pseudo posté: $_POST[pseudo] <br>";
+    echo "message posté: $_POST[message] <br>";
+}
+?>
+<hr>
+ 
+ <form method="post" action="">
+     <label for="pseudo">Pseudo</label><br>
+     <input type="text" id="pseudo" name="pseudo"><br>
+      
+     <label for="message">Message</label><br>
+     <textarea id="message" name="message" cols="50" rows="7"></textarea><br>
+      
+     <input type="submit" value="Envoyer le message">
+ </form> 
